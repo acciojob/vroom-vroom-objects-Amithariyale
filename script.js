d8965d4a896b445e73f05dfcd797d7a0d9655357
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-16 16:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	class Car {
+		constructor() {
+			this.make=make;
+			this.model=model;
+		}
+		getMakeModel(){
+			return `${this.make} ${this.model}`;
+		}
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	class SportsCar extends Car {
+		constructor() {
+			super(make,model);
+			this.topSpeed=topSpeed;
+		}
+		getTopSpeed(){
+			return this.topSpeed;
+		}
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
